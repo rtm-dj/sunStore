b1c5cb667b9fae544422d68a2e094bf2b9555b7c
Artem <artem@yugaev.ru>
2024-09-19 01:24:56 +0300

    init

---
+++ sunStore/src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <RouterView />
+</template>
+
+<style>
+body{
+  margin: 0;
+  padding: 0;
+  background: #FFFDEF;
+}
+</style>
---
+++ sunStore/src/views/HomeView.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <div class="left-part">
+      <div class="language">
+        <button class="lang">en</button>
+      </div>
+      <div class="logo">
+        <img src="" alt="">
+      </div>
+      <div class="bottom-action">
+        <span></span>
+        <button class="buy">Купить</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+
+</style>
