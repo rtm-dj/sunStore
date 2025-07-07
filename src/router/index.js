12e1fdc2932302a5b8fe458d40a18518d964d011
Artem <artem@yugaev.ru>
2025-07-08 01:34:21 +0300

    404 page added

--- src/router/index.js
+++ src/router/index.js
@@ -3,8 +3,8 @@ import HomeView from '../views/HomeView.vue'
 import ComponentsView from '../views/ComponentsView.vue'
 import AboutView from '../views/AboutCompanyView.vue'
 
-import NProgress from 'nprogress';
-import 'nprogress/nprogress.css';
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -28,6 +28,11 @@ const router = createRouter({
       path: '/buy',
       name: 'buy',
       component: () => import('../views/BuyView.vue')
+    },
+    {
+      path: '/:pathMatch(.*)*',
+      name: 'notFound',
+      component: () => import('../views/NotFound.vue')
     }
   ],
   scrollBehavior(to, from, savedPosition) {
@@ -36,16 +41,21 @@ const router = createRouter({
 })
 
 router.beforeEach((to, from, next) => {
-  NProgress.start();
-  NProgress.configure({ easing: 'ease', speed: 400 });
-  NProgress.configure({ showSpinner: false });
-  setTimeout(() => {
-    next();
-  }, 300)
-});
+  NProgress.start()
+  NProgress.configure({ easing: 'ease', speed: 400 })
+  NProgress.configure({ showSpinner: false })
+
+  if (to.matched.length === 0) {
+    next('/notFound')
+  } else {
+    setTimeout(() => {
+      next()
+    }, 300)
+  }
+})
 
 router.afterEach(() => {
-  NProgress.done();
-});
+  NProgress.done()
+})
 
 export default router
---
+++ src/views/NotFound.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="notFound">
+        <img src="../assets/logo.png" alt="" class="logo">
+        <div class="subtitle">
+            Ooops🥺<br>Кажется, такой страницы у нас нет
+        </div>
+        <img src="../assets/rabbit.png" alt="" class="rabbit">
+    </div>
+</template>
+
+
+<style scoped>
+    .notFound{
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        background-color: #FFFDEF;
+        height: 100svh;
+        gap: 40px;
+    }
+    .logo{
+        width: 400px;
+    }
+    .subtitle{
+        font-family: 'Montserrat';
+        text-align: center;
+        font-size: 35px;
+    }
+    .rabbit{
+        width: 200px;
+    }
+    @media (max-width: 600px){
+        .logo{
+            width: 200px;
+        }
+        .subtitle{
+            font-family: 'Montserrat';
+            text-align: center;
+            font-size: 20px;
+        }
+        .rabbit{
+            width: 100px;
+        }
+    }
+</style>
