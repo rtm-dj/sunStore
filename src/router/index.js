b30aca6dbe8574dc5056bda351c70e4327dcc226
Artem <artem@yugaev.ru>
2025-07-09 01:18:58 +0300

    bugs fixed

--- src/router/index.js
+++ src/router/index.js
@@ -6,43 +6,29 @@ import AboutView from '../views/AboutCompanyView.vue'
 import NProgress from 'nprogress'
 import 'nprogress/nprogress.css'
 
-// const router = createRouter({
-//   history: createWebHistory(import.meta.env.BASE_URL),
-//   routes: [
-//     {
-//       path: '/',
-//       name: 'home',
-//       component: HomeView
-//     },
-//     {
-//       path: '/components',
-//       name: 'components',
-//       component: ComponentsView
-//     },
-//     {
-//       path: '/about',
-//       name: 'about',
-//       component: AboutView
-//     },
-//     {
-//       path: '/buy',
-//       name: 'buy',
-//       component: () => import('../views/BuyView.vue')
-//     },
-//     {
-//       path: '/:pathMatch(.*)*',
-//       name: 'notFound',
-//       component: () => import('../views/NotFound.vue')
-//     }
-//   ],
-//   scrollBehavior(to, from, savedPosition) {
-//     return { top: 0 }
-//   }
-// })
-
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView
+    },
+    {
+      path: '/components',
+      name: 'components',
+      component: ComponentsView
+    },
+    {
+      path: '/about',
+      name: 'about',
+      component: AboutView
+    },
+    {
+      path: '/buy',
+      name: 'buy',
+      component: () => import('../views/BuyView.vue')
+    },
     {
       path: '/:pathMatch(.*)*',
       name: 'notFound',
@@ -53,6 +39,20 @@ const router = createRouter({
     return { top: 0 }
   }
 })
+
+// const router = createRouter({
+//   history: createWebHistory(import.meta.env.BASE_URL),
+//   routes: [
+//     {
+//       path: '/:pathMatch(.*)*',
+//       name: 'notFound',
+//       component: () => import('../views/NotFound.vue')
+//     }
+//   ],
+//   scrollBehavior(to, from, savedPosition) {
+//     return { top: 0 }
+//   }
+// })
 
 router.beforeEach((to, from, next) => {
   NProgress.start()
