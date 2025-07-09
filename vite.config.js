f28465b943d98357448e8beed5d2b1e4008e8d11
Artem <artem@yugaev.ru>
2025-07-09 03:37:18 +0300

    sitemap added

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,14 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
+import Pages from 'vite-plugin-pages'
+import Sitemap from 'vite-plugin-sitemap'
 
 export default defineConfig({
   plugins: [
     vue(),
+    Pages(),
+    Sitemap({ hostname: 'https://kladovayasun.ru' }),
   ],
   assetsInclude: ['**/*.glb'],
   base: '/',
