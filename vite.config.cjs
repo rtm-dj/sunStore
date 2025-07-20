9d8a4b7425f75895bedd936c322a188226f02135
Artem <artem@yugaev.ru>
2025-07-20 13:21:50 +0300

    v3 stable

--- vite.config.cjs
+++ vite.config.cjs
@@ -1,33 +1,41 @@
 const { fileURLToPath } = require('node:url')
 const { defineConfig } = require('vite')
 const vue = require('@vitejs/plugin-vue')
-// const Pages = require('vite-plugin-pages').default
-// const Sitemap = require('vite-plugin-sitemap').default
-
-const path = require('path')
 const prerender = require('vite-plugin-prerender')
+const path = require('path')
 
-module.exports = defineConfig({
-  plugins: [
-    vue(),
-    // Pages(),
-    // Sitemap({ hostname: 'https://kladovayasun.ru' }),
-    prerender({
-      staticDir: path.resolve(__dirname, 'dist'),
-      routes: [
-        '/',
-        '/about',
-        '/components',
-        '/buy'
-      ],
-      debug: true,
-    })
-  ],
-  assetsInclude: ['**/*.glb'],
-  base: '/',
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+module.exports = (async () => {
+  const { default: Sitemap } = await import('vite-plugin-sitemap')
+
+  return defineConfig({
+    plugins: [
+      vue(),
+      prerender({
+        staticDir: path.resolve(__dirname, 'dist'),
+        routes: [
+          '/',
+          '/about',
+          '/components',
+          '/buy'
+        ],
+        debug: true,
+      }),
+      Sitemap({
+        hostname: 'https://kladovayasun.ru/',
+        routes: [
+          '/',
+          '/about',
+          '/components',
+          '/buy'
+        ],
+      }),
+    ],
+    assetsInclude: ['**/*.glb'],
+    base: '/',
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
     }
-  }
-})
+  })
+})()
